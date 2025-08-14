b2c8dcaf66239acc6bc8721a43e4be8412173d69
usagi-nanban <so.yone2011@gmail.com>
2025-08-14 17:32:38 +0900

    Update and rename departure-bell-web-serial2-with-announce-autoplay-stop/main.js to departure-bell-web-serial2-with-announce-autoplay-stop/const CACHE_NAME = 'bell-app-v3'; const FILES_TO_CACHE = [   './',   './service-worker.js

---
+++ departure-bell-web-serial2-with-announce-autoplay-stop/const CACHE_NAME = 'bell-app-v3'; const FILES_TO_CACHE = [   './',   './service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'bell-app-v3';
+const FILES_TO_CACHE = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
